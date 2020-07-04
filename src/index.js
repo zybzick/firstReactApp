1fa2dcebf65fb1fcb100bf17d0257ab775591359
zybzick <arclanov94@gmail.com>
2020-07-04 18:46:04 +0500

    hello world app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom'
 
-const el = <h1>Hi</h1>
+const el = (
+    <div>
+        <h1>To do List</h1>
+        <input type="text" placeholder="search"/>
+        <ul>
+            <li>Do something</li>
+            <li>Repit</li>
+        </ul>
+    </div>
+);
 
 ReactDOM.render(el, document.getElementById('root'))
