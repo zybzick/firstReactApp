552be7775ac204be9836d59d2fdcc57aa799dbb9
zybzick <arclanov94@gmail.com>
2020-07-05 22:53:28 +0500

    importing css

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,17 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const SearchPanel = ()=>{
-    const searchText = 'search'
-    const searchStyle = {
-        fontSize: '20px'
-    }
-    return (
-        <input
-            type="text"
-            placeholder={searchText}
-            style={searchStyle}
-        />
-    )
-}
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
+};
 
 export default SearchPanel;
