9db8ac749d8f0108a79f85659f555693a0323236
zybzick <arclanov94@gmail.com>
2020-07-06 12:20:11 +0500

    add event listener on todo-list-item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,32 +4,38 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    onLabelClick = ()=> {
+        console.log(`${this.props.label}`)
+    }
+
     render() {
         const {label, important = false} = this.props
 
         const style = {
-            color: important ? 'steelblue' : 'black',
+            color: important ? 'steelblue' : 'light',
             fontWeight: important ? 'bold' : 'normal'
         };
 
         return (
             <span className="todo-list-item">
-      <span
-          className="todo-list-item-label"
-          style={style}>
-        {label}
-      </span>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={ this.onLabelClick }
+                >
+                  {label}
+                </span>
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation"/>
-      </button>
+            <button type="button"
+                    className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-exclamation"/>
+            </button>
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o"/>
-      </button>
-    </span>
+              <button type="button"
+                      className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash-o"/>
+              </button>
+        </span>
         );
     };
 }
